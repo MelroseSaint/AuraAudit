70830d0834cdc503b7305c273ec69c9076e8e911
MelroseSaint <monroedoses@gmail.com>
2025-12-23 17:17:00 -0500

    fix: add components index.ts with correct default exports

---
+++ components/index.ts
@@ -0,0 +1,3 @@
+export { default as DashboardShell } from './DashboardShell'
+export { default as AuditStreamingList } from './AuditStreamingList'
+export { default as ReputationScoreSkeleton } from './skeletons/ReputationScoreSkeleton'
